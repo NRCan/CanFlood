2dc51448e60e07d83afe2a02135f683a35447ca7
cefect <47092034+cefect@users.noreply.github.com>
2020-05-29 19:09:38 -0600

    Update oth_misc.py

--- canflood/build/oth_misc.py
+++ canflood/build/oth_misc.py
@@ -21,7 +21,7 @@ from scipy import interpolate, integrate
 start, ymd = datetime.datetime.now(), datetime.datetime.now().strftime('%Y%m%d')
 
 #==============================================================================
-# parametessr
+# pars---
 #==============================================================================
 l1 = ['False', 'FALSE', 'false', 'NO', 'No', 'no', 'N', 'n']
 l2 = ['True','TRUE','true', 'yes','YES','Yes', 'Y', 'y']
@@ -32,7 +32,7 @@ truefalse_d = {
 
 
 #==============================================================================
-# custom imports
+# custom imports------
 #==============================================================================
 
 #standalone runs
@@ -49,6 +49,8 @@ else:
     from hlpr.exceptions import QError as Error
 
 import hlpr.Q as hQ
+from hlpr.basic import view
+from model.dmg2 import DFunc
 
 
 mod_name = 'misc'
@@ -58,6 +60,8 @@ class Misc(hQ.Qcoms):
     # legacy index numbers
     legacy_ind_d = {0:'id1',1:'address',2:'id2',10:'class', 11:'struct_type', 13:'area', 
                     18:'bsmt_f', 19:'ff_height', 20:'lon',21:'lat', 25:'gel'}
+    
+    ctag_cLibN_d = None #curve tag location
     
 
     def __init__(self, 
@@ -77,6 +81,106 @@ class Misc(hQ.Qcoms):
         log = logger.getChild('crv_lib_smry')
         
         raise Error('gave up')
+    
+    def crv_consol(self, #scan a set of curve librarires and consolidate necessary curves
+                   fdf, #inventory data 
+                   cLib_all_d, #a set of curve librarires {libName:{ctag:curve_df}}
+                   logger=None):
+        #=======================================================================
+        # defaults
+        #=======================================================================
+        if logger is None: logger=self.logger
+        log = logger.getChild('crvCons')
+        mdf = pd.DataFrame()
+        #=======================================================================
+        # collect tags from inventory
+        #=======================================================================
+        boolcol = np.logical_and(
+            fdf.columns.str.contains('_tag'),
+            fdf.columns.str.startswith('f'))
+            
+        assert boolcol.any()
+        log.info('collecting tags from %i tagcolumns \n    %s'%(
+            boolcol.sum(),  fdf.columns[boolcol].tolist()))
+        
+        #loop these columns and collect tags
+        ftags_d = dict()
+        for coln, cser in fdf.loc[:, boolcol].items():
+            ftags_d[coln] = cser.dropna().unique().tolist()
+            
+            #collect tag count meta
+            mdf = mdf.join(cser.value_counts(dropna=False), how='outer')
+            
+        mdf['total'] = mdf.sum(axis=1, skipna=True).astype(np.int)
+        log.info('collected %i tags in inventory across all nests \n    %s'%(
+                 len(mdf), mdf['total']))
+        
+        #=======================================================================
+        # build index of curve locations
+        #=======================================================================
+        if self.ctag_cLibN_d is None:
+            d = dict()
+            for cLname, cLib_d in cLib_all_d.items():
+                for ctag, cdf in cLib_d.items():
+                    if ctag.startswith('_'):continue #skip these
+                    if not ctag in d:
+                        d[ctag] = [cLname]
+                    else:
+                        d[ctag].append(cLname)
+            
+            log.info('built index of %i curves from %i librarires'%(len(d), len(cLib_d)))
+            self.ctag_cLibN_d = d
+            """
+            view(pd.Series(d))
+            """
+        
+        ctagL_d = self.ctag_cLibN_d
+
+        #=======================================================================
+        # collect tags from curve library
+        #=======================================================================
+        ctags = set(mdf.index.dropna().tolist())
+        log.info('collecting %i ctags from %i loaded'%(len(ctags), len(ctagL_d)))
+        
+        #check coverage
+        s = ctags.difference(ctagL_d.keys())
+        if len(s)>0:
+            log.warning('%i finv tags not found in passed librarries \n    %s'%(len(s), s))
+        
+        
+        
+        res_lib = dict() #results container for curves
+               
+        #loop through each request and collect thre data
+        for ctag in ctags:
+            
+            #===================================================================
+            # get the lib names
+            #===================================================================
+            libNames = ctagL_d[ctag]
+            if len(libNames)>1:
+                log.warning('%s found in %i librarires... taking first\n    %s'%(
+                    ctag, len(libNames), libNames))
+                libName = libNames[0]
+            elif len(libNames)==1:
+                libName = libNames[0]
+            else:
+                raise Error('bad length')
+                
+            #===================================================================
+            # get the curve data
+            #===================================================================
+            cdf = cLib_all_d[libName][ctag]
+            
+            assert isinstance(cdf,  pd.DataFrame)
+            assert len(cdf.columns)==2
+            
+            
+            
+            res_lib[ctag] = cdf
+        
+        
+        
         
 def run2():
     """
@@ -95,10 +199,12 @@ def run2():
     cLib_dir=r'C:\LS\03_TOOLS\CanFlood\_ins\20200529\curves'
     
     runpars_d={
-        'sfd':{
-            'out_dir':os.path.join(out_dir, 'sfd'),
-            'finv_fp':os.path.join(data_dir, 'finv_tagSFD_01_20200522_pts.gpkg'),
-            },
+        #=======================================================================
+        # 'sfd':{
+        #     'out_dir':os.path.join(out_dir, 'sfd'),
+        #     'finv_fp':os.path.join(data_dir, 'finv_tagSFD_01_20200522_pts.gpkg'),
+        #     },
+        #=======================================================================
         'nrp':{
             'out_dir':os.path.join(out_dir, 'nrp'),
             'finv_fp':os.path.join(data_dir, 'finv_tagNRP_01_20200521_pts.gpkg'),
@@ -124,7 +230,8 @@ def run2():
     log.info('loading %i curveLibrarires found in %s'%(len(cLib_fns), cLib_dir))
     cLib_all_d = dict()
     for fn in cLib_fns:
-        cLib_d = pd.read_excel(os.path.join(cLib_dir, fn), sheet_name=None)
+        cLib_d = pd.read_excel(os.path.join(cLib_dir, fn), 
+                               sheet_name=None, header=None, index=False)
         
         basefn = os.path.splitext(os.path.split(fn)[1])[0].replace('curves_', '')
         log.info('    %s got %i tabs: %s'%(basefn, len(cLib_d), list(cLib_d.keys())))
