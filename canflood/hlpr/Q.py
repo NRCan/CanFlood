4b1568595a1b95b6ff6db5d0214b7add269e783d
cefect <47092034+cefect@users.noreply.github.com>
2020-06-11 16:01:40 -0600

    togrid work

--- canflood/hlpr/Q.py
+++ canflood/hlpr/Q.py
@@ -331,10 +331,7 @@ class Qcoms(basic.ComWrkr): #baseclass for working w/ pyqgis outside the native 
             
             self.mstore.addMapLayer(vlay)
             
-            #add spatial index
-            
 
-            
             vlay.setName(vlay_raw.name()) #reset the name
             
         else: 
@@ -397,7 +394,7 @@ class Qcoms(basic.ComWrkr): #baseclass for working w/ pyqgis outside the native 
             layname='df',
             
             logger=None, 
-            db_f = None,
+
             ):
         """
         performance enhancement over vlay_new_df
@@ -544,17 +541,7 @@ class Qcoms(basic.ComWrkr): #baseclass for working w/ pyqgis outside the native 
         
         return vlay
     
-    #===============================================================================
-    # LAYER WRITING  -------------------------------------------------------------------
-    #===============================================================================
-    
-    
-    
-    #===============================================================================
-    # LAYER WRITING  -------------------------------------------------------------------
-    #===============================================================================
-    
-    
+
     
 
     
@@ -1656,6 +1643,128 @@ class Qcoms(basic.ComWrkr): #baseclass for working w/ pyqgis outside the native 
         
         return 
 
+    def joinbylocationsummary(self,
+                                vlay, #polygon layer to sample from
+                                 join_vlay, #layer from which to extract attribue values onto th ebottom vlay
+                                 jlay_fieldn_l, #list of field names to extract from the join_vlay
+                                 jvlay_selected_only = False, #only consider selected features on the join layer
+
+                                 predicate_l = ['intersects'],#list of geometric serach predicates
+                                 smry_l = ['sum'], #data summaries to apply
+                                 discard_nomatch = False, #Discard records which could not be joined
+                                 
+                                 use_raw_fn=False, #whether to convert names back to the originals
+                                 layname=None,
+                                 
+                     ):
+        """
+        WARNING: This ressets the fids
+        
+        discard_nomatch: 
+            TRUE: two resulting layers have no features in common
+            FALSE: in layer retains all non matchers, out layer only has the non-matchers?
+        
+        """
+        #=======================================================================
+        # presets
+        #=======================================================================
+        algo_nm = 'qgis:joinbylocationsummary'
+        
+        predicate_d = {'intersects':0,'contains':1,'equals':2,'touches':3,'overlaps':4,'within':5, 'crosses':6}
+        summaries_d = {'count':0, 'unique':1, 'min':2, 'max':3, 'range':4, 'sum':5, 'mean':6}
+
+        log = self.logger.getChild('joinbylocationsummary')
+        
+        if isinstance(jlay_fieldn_l, set):
+            jlay_fieldn_l = list(jlay_fieldn_l)
+            
+            
+        #convert predicate to code
+        pred_code_l = [predicate_d[pred_name] for pred_name in predicate_l]
+            
+        #convert summaries to code
+        sum_code_l = [summaries_d[smry_str] for smry_str in smry_l]
+        
+        
+        if layname is None:  layname = '%s_jsmry'%vlay.name()
+            
+        #=======================================================================
+        # prechecks
+        #=======================================================================
+
+        
+        if not isinstance(jlay_fieldn_l, list):
+            raise Error('expected a list')
+        
+        #check requested join fields
+        fn_l = [f.name() for f in join_vlay.fields()]
+        s = set(jlay_fieldn_l).difference(fn_l)
+        assert len(s)==0, 'requested join fields not on layer: %s'%s
+                
+        #=======================================================================
+        # assemble pars
+        #=======================================================================
+        main_input=vlay
+
+        if jvlay_selected_only:
+            join_input = self._get_sel_obj(join_vlay)
+        else:
+            join_input = join_vlay
+
+        #assemble pars
+        ins_d = { 'DISCARD_NONMATCHING' : discard_nomatch,
+                  'INPUT' : main_input,
+                   'JOIN' : join_input,
+                   'JOIN_FIELDS' : jlay_fieldn_l,
+                  'OUTPUT' : 'TEMPORARY_OUTPUT', 
+                  'PREDICATE' : pred_code_l, 
+                  'SUMMARIES' : sum_code_l,
+                   }
+        
+        log.debug('executing \'%s\' with ins_d: \n    %s'%(algo_nm, ins_d))
+        
+
+        res_d = processing.run(algo_nm, ins_d, feedback=self.feedback)
+
+            
+        
+        res_vlay = res_d['OUTPUT']
+        
+        """
+        res_d.keys()
+        """
+        #===========================================================================
+        # post formatting
+        #===========================================================================
+        res_vlay.setName(layname) #reset the name
+        
+        #get new field names
+        nfn_l = set([f.name() for f in res_vlay.fields()]).difference([f.name() for f in vlay.fields()])
+        
+        #=======================================================================
+        # rename fields
+        #=======================================================================
+        if use_raw_fn:
+            assert len(smry_l)==1, 'rename only allowed for single sample stat'
+            rnm_d = {s:s.replace('_%s'%smry_l[0],'') for s in nfn_l}
+            
+            s = set(rnm_d.values()).symmetric_difference(jlay_fieldn_l)
+            assert len(s)==0, 'failed to convert field names'
+            
+            res_vlay = vlay_rename_fields(res_vlay, rnm_d, logger=log)
+            
+            nfn_l = jlay_fieldn_l
+        
+        
+        
+        log.info('sampled \'%s\' w/ \'%s\' (%i hits) and \'%s\'to get %i new fields \n    %s'%(
+            join_vlay.name(), vlay.name(), res_vlay.dataProvider().featureCount(), 
+            smry_l, len(nfn_l), nfn_l))
+        
+        return res_vlay, nfn_l
+
+
+    
     
     
     #==========================================================================
@@ -3149,6 +3258,46 @@ def vlay_get_bgeo_type(vlay,
             return gtype
         
     raise Error('failed to match')
+
+
+def vlay_rename_fields(
+        vlay_raw,
+        rnm_d, #field name conversions to apply {old FieldName:newFieldName}
+        logger=None,
+        feedback=None,
+        ):
+    
+    if logger is None: logger=mod_logger
+    log=logger.getChild('vlay_rename_fields')
+    
+    #get a working layer
+    vlay_raw.selectAll()
+    vlay = processing.run('native:saveselectedfeatures', 
+            {'INPUT' : vlay_raw, 'OUTPUT' : 'TEMPORARY_OUTPUT'}, 
+            feedback=feedback)['OUTPUT']
+    
+    #get fieldname index conversion for layer
+    fni_d = {f.name():vlay.dataProvider().fieldNameIndex(f.name()) for f in vlay.fields()}
+    
+    #re-index rename request
+    fiRn_d = {fni_d[k]:v for k,v in rnm_d.items()}
+
+    #apply renames
+    if not vlay.dataProvider().renameAttributes(fiRn_d):
+        raise Error('failed to rename')
+    vlay.updateFields()
+    
+    #check it
+    fn_l = [f.name() for f in vlay.fields()]
+    s = set(rnm_d.values()).difference(fn_l)
+    assert len(s)==0, 'failed to rename %i fields: %s'%(len(s), s)
+    
+    vlay.setName(vlay_raw.name())
+    
+    log.debug('applied renames to \'%s\' \n    %s'%(vlay.name(), rnm_d))
+    
+    return vlay
+
 
 #==============================================================================
 # type checks-----------------
--- canflood/results/togrid.py
+++ canflood/results/togrid.py
@@ -23,7 +23,7 @@ import pandas as pd
 
 #Qgis imports
 
-from qgis.core import QgsVectorLayer, QgsRasterLayer, QgsFeatureRequest, QgsProject
+from qgis.core import *
 
 
 
@@ -33,9 +33,9 @@ from qgis.core import QgsVectorLayer, QgsRasterLayer, QgsFeatureRequest, QgsProj
 
 from hlpr.exceptions import QError as Error
     
-from hlpr.Q import Qcoms
+from hlpr.Q import Qcoms, view, vlay_get_fdf, vlay_rename_fields, vlay_get_fdata
 
-
+import processing  
 
 
 #==============================================================================
@@ -60,7 +60,243 @@ class Gwrkr(Qcoms):
         
         self.logger.debug('init finished')
         
+    def load_grid(self, #load a grid layer and do some checks
+                  fp,
+                  gid = 'DA_id',#grid field name
+                  logger=None):
+        
+        if logger is None: logger=self.logger
+        log=logger.getChild('load_grid')
+
+        #=======================================================================
+        # load        
+        #=======================================================================
+        vlay = self.load_vlay(fp, logger=log)
+        
+        
+        #=======================================================================
+        # check
+        #=======================================================================
+        assert 'Polygon' in QgsWkbTypes().displayString(vlay.wkbType())
+        
+        dp = vlay.dataProvider()
+        
+        assert gid in [f.name() for f in vlay.fields()], 'missing gid \'%s\''%gid
+        
+
+        
+        log.info('loaded grid w/ %i cells and gid=\'%s\' \'%s\' '%(dp.featureCount(),gid, vlay.name()))
+        
+        self.gvlay = vlay
+        self.gid=gid
+        
+        return vlay
+    
+    
+    def rename_events(self, #rename events from the evals
+                      vlay_raw,
+                      ev_fp,
+                      repl_str = '_WL_fail_0415', #optional string to drop from fieldnames
+                      event_probs='ari', #format of evals
+                      logger=None,
+                      ):
+        if logger is None: logger=self.logger
+        log=logger.getChild('rnmE')
+        #=======================================================================
+        # get the evals
+        #=======================================================================
+        ev_ser = pd.read_csv(ev_fp).T.iloc[:,0].rename('evals').drop_duplicates()
+        ev_ser.index = ev_ser.index.str.replace(repl_str,'', case=False)
+        
+        #convert 
+        if event_probs=='ari':
+            ev_ser = 1/ev_ser
+        
+        #=======================================================================
+        # get conversions and check
+        #=======================================================================
+        rnm_d = ev_ser.astype(str).to_dict()
+        rnm_d = dict(zip(rnm_d.values(), rnm_d.keys()))
+        
+
+        fn_l = [f.name() for f in vlay_raw.fields()]
+
+        s = set(rnm_d.keys()).difference(fn_l)
+        assert len(s)==0, 'vlay fields mismatch evals fields: \n    %s'%s
+        
+        #=======================================================================
+        # rename the fields
+        #=======================================================================
+        vlay = vlay_rename_fields(vlay_raw, rnm_d, logger=log)
+        
+        log.info('renamed %i fields on \'%s\' from evals \n    %s'%(
+            len(rnm_d), vlay.name(), rnm_d))
+        return vlay
+
+        """
+        view(vlay)
+        """
+        
+        
+        
+        
+        
+        
+    def gsamp(self, #resample results to a grid (from single asset res layer)
+              avlay, #asset results layer
+              gvlay=None, #new polygon grid to sample
+              gid=None,
+              res_fnl = ['ead'], #list of result fields to downsample
+              use_raw_fn=True, #whether to convert the summary field names back to raw.
+              
+              logger=None,
+              discard_nomatch=False,
+              **jkwargs #joinbylocationsummary kwargs
+              ):
+        """
+        resample results 
+        """
+        
+        if logger is None: logger=self.logger
+        log=logger.getChild('gsamp')
+        
+        
+        if gvlay is None: gvlay = self.gvlay
+        if gid is None: gid=self.gid
+        
+        
+        
+        log.info('downsampling \'%s\' (%i feats) to \'%s\' (%i)'%(
+            avlay.name(), avlay.dataProvider().featureCount(), gvlay.name(),
+            gvlay.dataProvider().featureCount()))
+        
+        #=======================================================================
+        # prechecks
+        #=======================================================================
+        fn_l = [f.name() for f in gvlay.fields()]
+        assert gid in fn_l, gid
+        
+        fn_l = [f.name() for f in avlay.fields()]
+        s = set(res_fnl).difference(fn_l)
+        assert len(s)==0, 'missing requested results fields: %s'%res_fnl
+        
+        #check the gids
+        gid_d = vlay_get_fdata(gvlay, fieldn=gid, logger=log)
+        assert pd.Series(gid_d).is_unique
+        
+        #=======================================================================
+        # calc
+        #=======================================================================
+        gvlay1, nfn_l = self.joinbylocationsummary(gvlay, avlay, res_fnl, use_raw_fn=use_raw_fn,
+                                                   discard_nomatch=discard_nomatch,
+                                                   **jkwargs)
+        
+        #=======================================================================
+        # wrap
+        #=======================================================================
+        if not discard_nomatch:
+            assert gvlay1.dataProvider().featureCount()==gvlay.dataProvider().featureCount()
+        
+        
+        
+        return gvlay1, nfn_l
+    
+    def combine_gsamp(self, #combine a set of asset results to a grid
+                      avlay_d,
+                      res_fnl = ['ead'], #list of result fields to downsample
+                      gvlay=None,
+                      gid=None,
+                      rnm_d=dict(), #optional POST field name conversion. {old fieldName:new fieldName}
+                      logger=None,
+                      ):
+        #=======================================================================
+        # defaults
+        #=======================================================================
+        if logger is None: logger=self.logger
+        log=logger.getChild('cGsamp')
+        if gvlay is None: gvlay=self.gvlay
+        if gid is None: gid=self.gid
+        
+        
+        
+        #=======================================================================
+        # loop and collect grid totals for each inventory
+        #=======================================================================
+        res_d = dict()
+
+        for fclass, avlay in avlay_d.items():
+
+            log.info('downsampling \'%s\''%fclass)
+            
+            #sum on polys
+            rvlay, nfn_l = self.gsamp(avlay, res_fnl=res_fnl,
+                                      gid=gid, gvlay=gvlay, logger=log)
+            
+            #convert to frame
+            df = vlay_get_fdf(rvlay, logger=log).drop(['fid'], axis=1, errors='ignore').rename(
+                columns=rnm_d).set_index(gid).loc[:, res_fnl]
+                
+            #check it
+            assert df.index.is_unique, fclass
+            assert 'int' in df.index.dtype.name, fclass
+                
+            res_d[fclass] = df
+
+
+
+        log.info('collected totals from %i layers'%len(res_d))
+        
+        #=======================================================================
+        # combine-----
+        #=======================================================================
+
+        #empty results container
+        rdf = pd.DataFrame(
+            index=vlay_get_fdf(gvlay, logger=log).set_index(gid, drop=True).index,
+            columns=res_d[fclass].columns).fillna(0)
+            
+        mdf = pd.DataFrame(index=res_d.keys(), columns=rdf.columns)
+        
+        for fclass, df in res_d.items():
+            s = set(df.index).difference(rdf.index)
+            assert len(s)==0, fclass
+            
+            rdf1 = rdf.add(df, axis=0, fill_value=0)
+            
+            assert np.array_equal(rdf1.index, rdf.index)
+            assert np.array_equal(rdf1.columns, rdf.columns)
+            
+            bdf = rdf1>=rdf
+            assert bdf.all().all()
+            
+            rdf = rdf1
+            
+            #update meta
+            mdf.loc[fclass,: ] = rdf.sum(axis=0).to_frame().T
+            
+        log.info('totaled across %i asset layers on %i grids'%(
+            len(res_d), len(rdf)))
+        
+        #=======================================================================
+        # assemble results
+        #=======================================================================
+        
+        geo_d = vlay_get_fdata(gvlay, geo_obj=True, logger=log)
+        rvlay = self.vlay_new_df2(rdf, geo_d=geo_d, logger=log,
+                                  layname='%s_comb_%i'%(gvlay.name(), len(res_d)))
+        
+        return rvlay, res_d, mdf
+        """
+        view(rdf)
+        [f.name() for f in avlay.fields()]
+        view(rvlay)
+        """
+        
+        
+        
+    
+        
         
 
 
-        
+         
