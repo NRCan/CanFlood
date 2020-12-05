62d7c3a63e8db6ff582cefa70b8e5f063adb5e36
cefect <47092034+cefect@users.noreply.github.com>
2020-12-05 10:06:34 -0700

    main clean

--- canflood/CanFlood.py
+++ canflood/CanFlood.py
@@ -120,135 +120,142 @@ class CanFlood:
         return QCoreApplication.translate('CanFlood_inPrep', message)
 
 
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            # Adds plugin icon to Plugins toolbar
-            self.iface.addToolBarIcon(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
+#===============================================================================
+#     def add_action(
+#         self,
+#         icon_path,
+#         text,
+#         callback,
+#         enabled_flag=True,
+#         add_to_menu=True,
+#         add_to_toolbar=True,
+#         status_tip=None,
+#         whats_this=None,
+#         parent=None):
+#         raise Error('who is using this?')
+#         
+#         """Add a toolbar icon to the toolbar.
+# 
+#         :param icon_path: Path to the icon for this action. Can be a resource
+#             path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
+#         :type icon_path: str
+# 
+#         :param text: Text that should be shown in menu items for this action.
+#         :type text: str
+# 
+#         :param callback: Function to be called when the action is triggered.
+#         :type callback: function
+# 
+#         :param enabled_flag: A flag indicating if the action should be enabled
+#             by default. Defaults to True.
+#         :type enabled_flag: bool
+# 
+#         :param add_to_menu: Flag indicating whether the action should also
+#             be added to the menu. Defaults to True.
+#         :type add_to_menu: bool
+# 
+#         :param add_to_toolbar: Flag indicating whether the action should also
+#             be added to the toolbar. Defaults to True.
+#         :type add_to_toolbar: bool
+# 
+#         :param status_tip: Optional text to show in a popup when mouse pointer
+#             hovers over the action.
+#         :type status_tip: str
+# 
+#         :param parent: Parent widget for the new action. Defaults None.
+#         :type parent: QWidget
+# 
+#         :param whats_this: Optional text to show in the status bar when the
+#             mouse pointer hovers over the action.
+# 
+#         :returns: The action that was created. Note that the action is also
+#             added to self.actions list.
+#         :rtype: QAction
+#         """
+# 
+#         icon = QIcon(icon_path)
+#         action = QAction(icon, text, parent)
+#         action.triggered.connect(callback)
+#         action.setEnabled(enabled_flag)
+# 
+#         if status_tip is not None:
+#             action.setStatusTip(status_tip)
+# 
+#         if whats_this is not None:
+#             action.setWhatsThis(whats_this)
+# 
+#         if add_to_toolbar:
+#             # Adds plugin icon to Plugins toolbar
+#             self.iface.addToolBarIcon(action)
+# 
+#         if add_to_menu:
+#             self.iface.addPluginToMenu(
+#                 self.menu,
+#                 action)
+# 
+#         self.actions.append(action)
+# 
+#         return action
+#===============================================================================
 
     def initGui(self):
+        """
+        where is this called?
+        called on Qgis Load?
+        add UI elements to Qgis
+        """
         
 
         #=======================================================================
         # add toolbar
         #=======================================================================
         """Create the menu entries and toolbar icons inside the QGIS GUI."""  
-        self.toolbar = self.iface.addToolBar('CanFlood')
+        self.toolbar = self.iface.addToolBar('CanFlood') #build a QToolBar
         self.toolbar.setObjectName('CanFloodToolBar')
         
         #=======================================================================
         # button 1: Build
         #=======================================================================
-        self.toolbarProjectDataPrep = QAction(QIcon(
+        #build the button
+        self.button_build = QAction(QIcon(
             ':/plugins/canflood_inprep/icons/Andy_Tools_Hammer_Spanner_23x23.png'), 
             'Build', self.iface.mainWindow())
          
-        self.toolbarProjectDataPrep.setObjectName('Build')
-        self.toolbarProjectDataPrep.setCheckable(False)
-        self.toolbarProjectDataPrep.triggered.connect(self.showToolbarDataPrep)
-        self.toolbar.addAction(self.toolbarProjectDataPrep)
+        self.button_build.setObjectName('Build')
+        self.button_build.setCheckable(False)
+        self.button_build.triggered.connect(self.showToolbarDataPrep)
+        
+        
+        self.toolbar.addAction(self.button_build)
 
         #=======================================================================
         # button 2: Model
         #=======================================================================
-        self.toolbarProjectModelling = QAction(
+        self.button_model = QAction(
             QIcon(':/plugins/canflood_inprep/icons/house_flood.png'),
             'Model', self.iface.mainWindow())
         
-        self.toolbarProjectModelling.setObjectName('Model')
-        self.toolbarProjectModelling.setCheckable(False)
-        self.toolbarProjectModelling.triggered.connect(self.showToolbarProjectModelling)
-        self.toolbar.addAction(self.toolbarProjectModelling)
+        self.button_model.setObjectName('Model')
+        self.button_model.setCheckable(False)
+        self.button_model.triggered.connect(self.showToolbarProjectModelling)
+        self.toolbar.addAction(self.button_model)
 
         #=======================================================================
         # button 3: Results
         #=======================================================================
-        self.toolbarProjectResults = QAction(
+        self.button_results = QAction(
             QIcon(':/plugins/canflood_inprep/icons/eye_23x23.png'), 
             'Results', self.iface.mainWindow())
         
-        self.toolbarProjectResults.setObjectName('toolbarProjectResults')
-        self.toolbarProjectResults.setCheckable(False)
-        self.toolbarProjectResults.triggered.connect(self.showToolbarProjectResults)
-        self.toolbar.addAction(self.toolbarProjectResults)
+        self.button_results.setObjectName('button_results')
+        self.button_results.setCheckable(False)
+        self.button_results.triggered.connect(self.showToolbarProjectResults)
+        self.toolbar.addAction(self.button_results)
         
         #=======================================================================
-        # plugin reloadert
+        # add menu
         #=======================================================================
-        #=======================================================================
-        # from .build import lisamp, rsamp, oth_rfda
-        # from .hlpr import basic,  exceptions, logr,  plug, Q
-        # from .model import dmg2, modcom, risk1, risk2
-        # from .results import djoin, riskPlot
-        #=======================================================================
+        
 
         
 
