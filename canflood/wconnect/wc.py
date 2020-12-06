fb65bdf039dee02e9fdb67790aa2717cccfaa31b
cefect <47092034+cefect@users.noreply.github.com>
2020-12-05 19:54:02 -0700

    adapt code1

--- canflood/wconnect/wc.py
+++ canflood/wconnect/wc.py
@@ -11,7 +11,9 @@ web connections
 # imports---------------------------
 #==============================================================================
 #python standards
-import configparser, os, logging
+import os, logging
+
+from configparser import ConfigParser
 
 
 
@@ -31,12 +33,180 @@ class WebConnect(ComWrkr):
         self.iface=iface
         self.logger=plogger(self)
         super().__init__(logger=self.logger, **kwargs) #initilzie teh baseclass
+        
+        #setup
+        config_fp = os.path.abspath(__file__)[:-59]+"QGIS\QGIS3.ini" # The path to the configuration file for QGIS
+        assert os.path.exists(config_fp), 'bad config_fp: %s'%config_fp
+        self.config_fp = config_fp
     
     
     def addAll(self): #add all connections
         log = self.logger.getChild('addAll')
         
         log.push('addAll executed')
+        
+        
+        serv_d =  {#title: {serverType, url}}
+            'NRPI':('arcgismapserver','https://maps-cartes.ec.gc.ca/arcgis/rest/services/NPRI_INRP/NPRI_INRP/MapServer')
+            }
+    
+        #=======================================================================
+        # loop and add
+        #=======================================================================
+        for title, (serverType, url) in serv_d.items():
+            self.saveLayer(title, url, serverType)
+            
+            
+        log.info('added %i connections'%len(serv_d))
+            
+    
+    def get_settings(self, serverType, title, url):
+        
+        
+        
+        if (serverType == "WMS"): 
+            base_settings = "connections-wms\\"+title+"\\"
+            base_security_settings = "WMS\\"+title+"\\"
+            settings = [base_settings+"url",base_settings+"ignoreAxisOrientation",base_settings+"invertAxisOrientation",
+            base_settings+"ignoreGetMapURI",base_settings+"smoothPixmapTransform",base_settings+"dpimode",base_settings+"referer",
+            base_settings+"ignoreGetFeatureInfoURI",base_security_settings+"username",base_security_settings+"password",base_security_settings+"authcfg"]
+            
+            settings_ans = [url,"false","false","false","false","7","","false","","",""]
+            
+        elif (serverType == "WFS"):
+            base_settings = "connections-wfs\\"+title+"\\"
+            base_security_settings = "WFS\\"+title+"\\"
+            settings = [base_settings+"url",base_settings+"ignoreAxisOrientation",base_settings+"invertAxisOrientation",
+            base_settings+"version",base_settings+"maxnumfeatures",base_security_settings+"username",
+            base_security_settings+"password",base_security_settings+"authcfg"]
+            
+            settings_ans = [url,"false","false","auto","","","",""]
+        
+        elif (serverType == "arcgismapserver"):
+            base_settings = "connections-arcgismapserver\\"+title+"\\"
+            base_security_settings = "arcgismapserver\\"+title+"\\"
+            settings = [base_settings+"url",base_security_settings+"username",base_security_settings+"password",
+            base_security_settings+"authcfg"]
+            
+            settings_ans = [url,"","",""]
+            
+        else:
+            raise Error('unrecognized serverType: %s'%serverType)
+            
+        return settings, settings_ans, base_settings
+        
+        
+        
+    def saveLayer(self, #add entry to the ini file
+                   title,url,serverType):
+        """taken from canadian_web_services
+        
+        is there an api method to do this?
+        """
+        
+        '''
+        Standalone function that saves services into the registry 
+        @param title - The title of the service
+        @param url - the url of the service
+        @param serverType - the serverType of service (ie WMS,WFS,ESRI MapServer)
+        '''
+        
+        log = self.logger.getChild('saveLayer')
+        
+        log.info('on %s'%title)
+        filepath =  self.config_fp
+        #=======================================================================
+        # open up the ini file
+        #=======================================================================
+        config = configparser.ConfigParser()
+
+        try: # Check to see if we have already opened the configuration file
+            config.read(self.config_fp)
+        except: # Does nothing if we already have opened it
+            pass
+        
+        #=======================================================================
+        # calculate the settings
+        #=======================================================================
+        settings, settings_ans, base_settings = self.get_settings(serverType, title, url)
+
+            
+        #=======================================================================
+        # see if its been written
+        #=======================================================================
+        try: # try block checks if the service has been added
+            """bad way to do this...."""
+            check = config["qgis"][base_settings+"url"] # Checks if the service has already been added
+            already_added = config["qgis"][base_settings+"url"] == url # checks to see if the urls match (Used since we might encounter services with the same name but different urls)
+            
+        except: # If it hasn't add it 
+            for i in range (len(settings)):
+                config["qgis"][settings[i]] = settings_ans[i]
+            
+            with open(filepath,"w") as configfile:
+                config.write(configfile)
+                
+            already_added = True
+            
+            
+        log.info('already_added: %s'%already_added)
+        
+        if not already_added: # If the service has still not been added (most common reason to be here is if there are multiple services with the same name (title))
+            already_added,title = self.name_finder(title,config,serverType,url,0)
+            if(not(already_added)): # If the service has not already been added 
+                return # Do nothing
+            
+            else: # Otherwise add it to the configuration file
+                
+                settings, settings_ans, base_settings = self.get_settings(serverType, title, url)
+                
+                for i in range (len(settings)): # Sets information into config
+                    config["qgis"][settings[i]] = settings_ans[i]
+                
+                with open(filepath,"w") as configfile: # writes into file 
+                    config.write(configfile)    
+              
+        log.info('finished')      
+        return
+    
+    
+    def name_finder(self,
+                        title,config,serverType,url,counter):
+        
+        '''
+        Helper function for method saveLayers that obtains how we should name our service (Created to counter-act the fact that many services in the plugin have the same name)
+        
+        @param title - title of the service
+        @param config - ConfigParser that contains a dictionary like structure that contains the configuration settings of QGIS
+        @param serverType - the serverType of service (ie WMS, WFS, ESRI MapServer)
+        @param counter - Amount of times we have run this helper function for the given service
+        
+        @return bool - that is false if a write is not necessary, and true if it is
+        @return title - the new title we should be using when writing to the configuration settings file.  
+        '''
+        
+        try:# Checks if the service exists
+            counter += 1
+            
+            if (serverType == "WMS"): # If block checks what serverType of service we are given and does the appropriate url check
+                check = config["qgis"]["connections-wms\\"+title+"\\url"] == url
+            elif (serverType == "WFS"):
+                check = config["qgis"]["connections-wfs\\"+title+"\\url"] == url
+            elif (serverType == "ESRI MapServer"):
+                check = config["qgis"]["connections-arcgismapserver\\"+title+"\\url"] == url
+            
+            if(check): # If we found the service 
+                return False,title 
+            else: # Otherwise 
+            
+                if (counter == 1): # If block that deals with naming conventions, if this is the first time we ran the function 
+                    title += "_"+str(counter) 
+                else: # For every subsequent run of the function, remove what was previously added, and then add the new version Ex: Given title_1, strips the 1 and add 2 leaving us with : title_2  
+                    title = title[:-(len(str(counter - 1)))]
+                    title += str(counter)
+                return self.name_finder(title,config,serverType,url,counter)
+        except: # If the service doesn't exist, that means we can now write to the configuration settings file without error
+            return True,title 
         
         
         
