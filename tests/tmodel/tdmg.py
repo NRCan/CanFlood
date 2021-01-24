ced96adae518bccd7593e8e609d11613493d909e
cefect <47092034+cefect@users.noreply.github.com>
2021-01-23 17:00:58 -0700

    tdmg working

--- tests/tmodel/tdmg.py
+++ tests/tmodel/tdmg.py
@@ -24,52 +24,42 @@ also there are no class-level fixtures (i.e., all the data would have to be re-l
 '''
 
 
-import unittest, tempfile
-#import nose
-#from nose import tools
+import unittest, tempfile, inspect, logging, os, fnmatch
 
-#from nose.loader import TestLoader
+import pandas as pd
+import numpy as np
+
+
 from unittest import TestLoader
 
 from model.dmg2 import Dmg2
 
 
-#===============================================================================
-# class Dummy(object):
-#     def __init__(self,
-#                  cf_fp='',
-#                  name='dummyName'):
-#         self.cf_fp=cf_fp
-#         self.name=name
-#         
-#         print('init \'%s\' w/ %s'%(self.__class__.__name__, self.name))
-#         
-#         
-#     def foo(self):
-#         print('yea!')
-#===============================================================================
+"""just using the default console logger"""
+mod_logger = logging.getLogger('testing')
+
+
 
 
 class tModel(unittest.TestCase): #common model level testing methods
     
     def __init__(self, *args, 
-                 wrkr=None,
-                 #==============================================================
-                 # name='caseName',
-                 # cf_fp='',
-                 #==============================================================
+                 Model=None, #model object to test
+                 tdata_d = None, #container of data to test against
+
                  **kwargs):
-        #attach passed
+
         #=======================================================================
-        # self.cf_fp = cf_fp
-        # self.name = name
+        # attach
         #=======================================================================
-        
-        #print(type(wrkr))
-        self.wrkr=wrkr
+        self.Model=Model
+        self.tdata_d = tdata_d
+        self.name=Model.tag
+
+        assert inspect.isclass(type(Model))
         
         print('init \'%s\' w/ \'%s\' \n    args: %s'%(
-            self.__class__.__name__, self.wrkr.name,   args))
+            self.__class__.__name__, self.Model.tag,   args))
         
 
         super().__init__(*args, **kwargs) #initilzie the baseclass cascade
@@ -77,82 +67,151 @@ class tModel(unittest.TestCase): #common model level testing methods
     #===========================================================================
     # expected handler methods---------
     #===========================================================================
-    @classmethod
-    def setupClass(self):
-        print('setting up class \'%s\''%self.__class__.__name__)
-        
         
     def setUp(self):
-        print('setting up %s (%s) %s'%(self.__class__.__name__, self.wrkr.name, self._testMethodName))
+        print('setting up %s (%s) %s'%(self.__class__.__name__, self.Model.tag, self._testMethodName))
     def tearDown(self):
         print('tearing down\n')
         
-    def runTest(self): #called by the suite
-        print('runTest')
-        
-    
         
     #===========================================================================
     # test methods---------
     #===========================================================================
-    def test_main(self):
-        print('test_main w/ \'%s\''%self.wrkr.name)
-        self.wrkr.foo()
-        self.assertTrue(False)
-        
-    def test_one(self):
-        print('test_one')
-        self.assertTrue(True)
+
 
         
     
 #@tools.nottest
-class tDmg(tModel):
+class tDmg(tModel): #worker for testing the damage model
+    
+    
+    
     def __init__(self, *args, **kwargs):
+        """called for each test METHOD"""
         super().__init__(*args, **kwargs) #init baseclass
     
-    def test_parent1(self):
-        print('test parent1 on \"%s\''%self.wrkr.name)
-        self.assertTrue(False)
+    def test_main(self, #testing the main output
+                  dtag='dmgs',
+                  ): 
+        print('test_main on \'%s\''%self.name)
+        
+        #=======================================================================
+        # #check test data is available
+        #=======================================================================
+        assert dtag in self.tdata_d, 'missing required test data \'%s\''%dtag
+        chk_df = self.tdata_d[dtag].copy()
+        #=======================================================================
+        # #run the model
+        #=======================================================================
+        res_df = self.Model.run()
+        
+        #=======================================================================
+        # #do the checking        
+        #=======================================================================
+        self.assertIsInstance(res_df, pd.DataFrame)
+        
+        self.assertEqual(res_df.shape, chk_df.shape, msg='%s'%self.name)
+        
+        self.assertEqual(res_df.sum().sum(), chk_df.sum().sum(),  
+                         msg='%s data failed to sum'%self.name)
+        
+        
+        
+        
+    def test_attrim(self,
+                    dtag = 'attr02'):
+        print('test_attrim on \'%s\''%self.name)
+        #=======================================================================
+        # #check test data is available
+        #=======================================================================
+        assert dtag in self.tdata_d, 'missing required test data \'%s\''%dtag
+        chk_df = self.tdata_d[dtag].copy()
+        #=======================================================================
+        # #run the model
+        #=======================================================================
+        #run the mode
+        res_df = self.Model.run()
+        """have to re-run the model :("""
+        
+        #generate attributions
+        atr_dxcol = self.Model.get_attribution(res_df)
+        
+        #=======================================================================
+        # #do the checking        
+        #=======================================================================
+        self.assertIsInstance(atr_dxcol, pd.DataFrame)
+        self.assertIsInstance(atr_dxcol.columns, pd.MultiIndex)
+        
+        self.assertEqual(atr_dxcol.shape, chk_df.shape, msg='%s'%self.name)
+        
+        self.assertEqual(atr_dxcol.sum().sum(), chk_df.sum().sum(),  
+                         msg='%s data failed to sum'%self.name)
+        
+        
+
+        
+        
+#===========================================================================
+# results loading functions--------
+#===========================================================================
+"""not called during tests"""
+def load_test_data(
+                   res_dir, #directory containing all the results files
+                   dataLoad_pars = {'attr02*':{'header':[0,1], 'index_col':0}, 
+                                    'dmgs*':{'index_col':0}}
+                    ):
+    
+    d = dict()
+    
+    #get all csv files in the folder
+    allfns_l = [e for e in os.listdir(res_dir) if e.endswith('.csv')]
+    
+    #loop and load for each search
+    for searchPattern, load_kwargs in dataLoad_pars.items():
+        match_l = fnmatch.filter(allfns_l, searchPattern) #get those matching the search
+        assert len(match_l)==1, 'got multiple matches for \'%s\''%searchPattern
+        
+        #load the data
+        fp = os.path.join(res_dir, match_l[0])
+        d[searchPattern[:-1]] = pd.read_csv(fp, **load_kwargs)
+        
+        print('loaded \'%s\'  w/ %s'%(searchPattern, str(d[searchPattern[:-1]].shape)))
+        
+    
+    return d
+        
+        
+
+        
 
 
 
-
-#===============================================================================
-# def get_suite1(suitePars_d, #build the tDmg testing suite from a set of paramters
-#                 ):
-#     
-#     cases_d = dict()
-#     for testName, d in suitePars_d.items():
-#         cases_d[testName] = tDmg(name=testName, cf_fp=d['cf_fp'])
-#         
-#         #=======================================================================
-#         # unittest.TestLoader().loadTestsFromTestCase(tDmg)
-#         # pass
-#         #=======================================================================
-#     
-#     return unittest.TestSuite(cases_d.values())
-#===============================================================================
 
 
 
 def get_suite(suitePars_d, #build the tDmg testing suite from a set of paramters
+              absolute_fp=True,
+              attribution=True,
                 ):
     
     suite = unittest.TestSuite() #start the suite container
     
     for testName, d in suitePars_d.items():
 
-        wrkr = Dmg2(d['cf_fp'], 
+        #setup the model to test
+        Model = Dmg2(d['cf_fp'], 
                     out_dir=tempfile.mkdtemp(), #get a dummy temp directory
                     logger=mod_logger.getChild(testName), 
-                    tag=tag, 
+                    tag=testName, 
                      absolute_fp=absolute_fp, attriMode=attribution,
                      )._setup()
+                     
+        #load the check data
+        tdata_d = load_test_data(d['res_dir'])
         
         #build a test for each mathing method in the class
         for tMethodName in TestLoader().getTestCaseNames(tDmg):
-            suite.addTest(tDmg(tMethodName, wrkr=wrkr))
+            suite.addTest(tDmg(tMethodName, Model=Model, tdata_d=tdata_d))
 
     print('built suites')
     return suite
@@ -166,11 +225,11 @@ if __name__ == '__main__':
     runpars_d={
         'tut2_01.b01':{
              'cf_fp':r'C:\LS\03_TOOLS\CanFlood\_git\tests\_data\tut2_01\b01\CanFlood_tut2b_20210123.txt',
-             #'res_dir':r'C:\LS\03_TOOLS\CanFlood\_git\tests\_data\tut2_01\b01\dmg2',
+             'res_dir':r'C:\LS\03_TOOLS\CanFlood\_git\tests\_data\tut2_01\b01\dmg2',
              }, 
         'tut2_01.a01':{
              'cf_fp':r'C:\LS\03_TOOLS\CanFlood\_git\tests\_data\tut2_01\a01\CanFlood_tut2a_20210123.txt',
-             #'res_dir':r'C:\LS\03_TOOLS\CanFlood\_git\tests\_data\tut2_01\b01\dmg2',
+             'res_dir':r'C:\LS\03_TOOLS\CanFlood\_git\tests\_data\tut2_01\a01\dmg2',
              }, 
              }
     
