f17f123f6667a50246ff37468095b7d9c06f9a3a
cefect <47092034+cefect@users.noreply.github.com>
2021-03-02 18:23:30 -0700

    fix compare test

--- tests/tRes/tCompare.py
+++ tests/tRes/tCompare.py
@@ -42,7 +42,7 @@ class tObj(tRes): #worker for testing the damage model
     def test_cf(self, #testing the main output
 
                   ): 
-        print('test_noFail on \'%s\''%self.name)
+        print('test_cf on \'%s\''%self.name)
         
         mdf = self.Model.cf_compare()
         self.assertEqual(mdf.columns[-1], 'compare', msg=self.name)
@@ -50,6 +50,7 @@ class tObj(tRes): #worker for testing the damage model
 
         
     def test_riskCurves(self):
+        print('test_riskCurves on \'%s\''%self.name)
         for y1lab in ['AEP', 'impacts']:
             fig = self.Model.riskCurves(y1lab=y1lab)
             self.assertIsInstance(fig, plt.Figure,  msg='%s'%self.name)
@@ -59,28 +60,36 @@ class tObj(tRes): #worker for testing the damage model
         
 
 def gen_suite(
-
     runpars_d={
         'tut2':{
-            'cf_fp':r'tut2_01\a01\CanFlood_tut2a_20210123.txt',
+            'cf_fp':r'compare\CanFlood_tut2a_20210123.txt',
             'fps_d':{
-                 'tut2_01.a01':r'tut2_01\a01\CanFlood_tut2a_20210123.txt',
-                 'tut2_01.b01':r'tut2_01\b01\CanFlood_tut2b_20210123.txt',
-                'tut2_01.c01.mutEx':r'tut2_01\c01\CanFlood_tut2c_20210123_mutEx.txt',
+                 'tut2_01.a01':r'compare\a01\CanFlood_tut2a_20210123.txt',
+                 'tut2_01.b01':r'compare\b01\CanFlood_tut2b_20210123.txt',
+ 
                 },
             'res_dir':None,
             },
-        'tut2_short':{ #nice to test only 2 control files
-            'cf_fp':r'tut2_01\a01\CanFlood_tut2a_20210123.txt',
-            'fps_d':{
-                 'tut2_01.a01':r'tut2_01\a01\CanFlood_tut2a_20210123.txt',
-                 'tut2_01.b01':r'tut2_01\b01\CanFlood_tut2b_20210123.txt',
-                },
-            'res_dir':None,
-            }
+        #=======================================================================
+        # 'tut2_short':{ #nice to test only 2 control files
+        #     'cf_fp':r'tut2_01\a01\CanFlood_tut2a_20210123.txt',
+        #     'fps_d':{
+        #          'tut2_01.a01':r'tut2_01\a01\CanFlood_tut2a_20210123.txt',
+        #          'tut2_01.b01':r'tut2_01\b01\CanFlood_tut2b_20210123.txt',
+        #         },
+        #     'res_dir':None,
+        #     }
+        #=======================================================================
         }
 
     ):
+    """
+    Note on filepaths:
+        test modules specify a base_dir higher than the control file
+    however, for methods that spawn scenarios (e.g., compare)
+        teh base_dir is always the location of the control file
+    
+    """
     
     return get_suite(runpars_d,tObj,
                       dataLoad_pars = {},
