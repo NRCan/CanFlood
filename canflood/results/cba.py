30b98eef1c4cab15acb972387c58d81244099c75
cefect <47092034+cefect@users.noreply.github.com>
2021-03-16 13:08:32 -0600

    clarify cba dependencies

--- canflood/results/cba.py
+++ canflood/results/cba.py
@@ -21,10 +21,30 @@ import numpy as np
 import pandas as pd
 
 
+
+
+#==============================================================================
+# dependency check
+#==============================================================================
+# Let users know if they're missing any of our hard dependencies
+hard_dependencies = ("openpyxl",)
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append("{0}: {1}".format(dependency, str(e)))
+
+if missing_dependencies:
+    raise ImportError(
+        "Unable to import required dependencies:\n" + "\n".join(missing_dependencies)
+    )
+    
+del hard_dependencies, dependency, missing_dependencies
+
+
 import openpyxl
-
-
-
 
 #===============================================================================
 # customs
