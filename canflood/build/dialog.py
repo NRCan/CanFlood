0c82064c545e410e808a1501f2d0040877c1fac5
cefect <47092034+cefect@users.noreply.github.com>
2021-10-20 21:24:47 +0200

    more rPrep

--- canflood/build/dialog.py
+++ canflood/build/dialog.py
@@ -374,11 +374,9 @@ class BuildDialog(QtWidgets.QDialog, FORM_CLASS, hlpr.plug.QprojPlug):
         #=======================================================================
         self.pushButton_HSgenerate.clicked.connect(self.run_rsamp)
 
-        """TODO: connect this to a new dialog
-        self.pushButton_HS_rprep.clicked.connect(self.run_rPrep)"""
-        
-                #=======================================================================
-        # vfunc
+
+        #=======================================================================
+        # Raster Prep
         #=======================================================================
         #give commmon widgets
         for wName in self.RPrepDialog.inherit_atts:
@@ -389,9 +387,11 @@ class BuildDialog(QtWidgets.QDialog, FORM_CLASS, hlpr.plug.QprojPlug):
         def rpDia(): #helper to connect slots and 
             """only executing setup once called to simplify initial loading"""
             _ = self.RPrepDialog._setup()
+            #self.RPrepDialog.pushButton_HS_rprep.clicked.connect(self.run_rPrep)
+            
             self.RPrepDialog.show()
             
-        self.pushButton_HS_rprep.clicked.connect(vDia)
+        self.pushButton_HS_rprep.clicked.connect(rpDia)
 
         #======================================================================
         # TAB: EVENT VARIABLES---------
@@ -820,8 +820,10 @@ class BuildDialog(QtWidgets.QDialog, FORM_CLASS, hlpr.plug.QprojPlug):
         log.info('start \'run_rPrep\' at %s'%start)
  
         
-        
-        
+        """the buttons have been moved onto the sub-dialog
+        but we're keeping all the functions here for better integration
+        treats the popout as more of an extension"""
+        subDia = self.RPrepDialog 
         #=======================================================================
         # assemble/prepare inputs
         #=======================================================================
@@ -830,10 +832,10 @@ class BuildDialog(QtWidgets.QDialog, FORM_CLASS, hlpr.plug.QprojPlug):
         aoi_vlay = self.aoi_vlay
 
         #raster prep parameters
-        clip_rlays = self.checkBox_HS_clip.isChecked()
-        allow_download = self.checkBox_HS_dpConv.isChecked()
-        allow_rproj = self.checkBox_HS_rproj.isChecked()
-        scaleFactor = self.doubleSpinBox_HS_sf.value()
+        clip_rlays = self.subDia.checkBox_HS_clip.isChecked()
+        allow_download = self.subDia.checkBox_HS_dpConv.isChecked()
+        allow_rproj = self.subDia.checkBox_HS_rproj.isChecked()
+        scaleFactor = self.subDia.doubleSpinBox_HS_sf.value()
 
         
         #=======================================================================
--- canflood/build/rprep_dialog.py
+++ canflood/build/rprep_dialog.py
@@ -29,6 +29,7 @@ FORM_CLASS, _ = uic.loadUiType(ui_fp)
 
 
 class RPrepDialog(QtWidgets.QDialog, FORM_CLASS, hlpr.plug.QprojPlug):
+    inherit_atts =  [] #for passing from the main
     def __init__(self, iface, parent=None, **kwargs):
         super(RPrepDialog, self).__init__(parent) #only calls QtWidgets.QDialog
         
@@ -36,8 +37,16 @@ class RPrepDialog(QtWidgets.QDialog, FORM_CLASS, hlpr.plug.QprojPlug):
         
         self.setupUi(self)
         
+    def _setup(self):
+        """
+        called on launch
+        """
+
         self.connect_slots()
         
         
     def connect_slots(self):
+        """leaving this on the main
+        self.pushButton_HS_rprep.clicked.connect(self.run_rPrep)
+        """
         pass
