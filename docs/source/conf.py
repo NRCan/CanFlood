a886317be969d5c1e25af2140650b3f9d005c816
Dhanyatha Harish <dhanyatha.harish@ibigroup.com>
2021-10-21 13:48:55 -0400

    Generated po file and added partial translated content and built an html file manually to save a sample copy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,3 +65,7 @@ html_static_path = ['_static']
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = "en"
+
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_uuid = True
+gettext_compact = False 
