2e15ef1ebea5579b7150d2cddb7c026df4cacd25
cefect <cefect@gmail.com>
2021-12-29 14:43:58 +0100

    Create vmisc.py

---
+++ tools/vfunc_conv/vmisc.py
@@ -0,0 +1,93 @@
+'''
+Created on Dec. 29, 2021
+
+@author: cefect
+
+miscellaneous tools for dealing with vfuncLibs
+'''
+
+
+#==============================================================================
+# imports------------
+#==============================================================================
+import os, datetime
+start =  datetime.datetime.now()
+import numpy as np
+import pandas as pd
+
+from hlpr.basic import view, force_open_dir
+from hlpr.exceptions import Error
+
+from vfunc_conv.vcoms import VfConv
+from model.dmg2 import Dmg2
+
+
+import wFlow.scripts 
+
+class Session(VfConv, Dmg2, wFlow.scripts.Session):
+    def __init__(self,
+                 projName='vfMisc',
+                 **kwargs):
+        
+        super().__init__(projName=projName,
+                         **kwargs) 
+
+
+
+
+ 
+
+def add_smry( #add a summary page to a vfunc library
+        fp=r'C:\LS\02_WORK\IBI\202107_OBWB\04_CALC\curves\IBI2015\rfda_2017_dmg_20210205\RFDA2017_dmg_210205.xls',
+        ftags_valid=None, #optional tab names to include
+        ):
+    
+    
+    
+    with Session( write=True, plot=False) as ses:
+        
+        #load the data
+        cdf_d = ses.load_data(fp)
+        
+        assert not '_smry' in cdf_d
+        
+        #get ftags
+        """setup_dfuncs is configured to only load those dfuncs
+        found in the finv... this is ignored when ftags_valid is passed"""
+        if ftags_valid is None:
+            ftags_valid = list(cdf_d.keys())
+        
+        #build the dfuncs
+        ses.setup_dfuncs(cdf_d, ftags_valid=ftags_valid)
+        
+        #retreive
+        dfuncs_d = ses.dfuncs_d
+        clib_d = {k:v.df_raw for k,v in dfuncs_d.items()} #get loaded frames
+        
+        #generate the summary frame
+        smry_df = pd.DataFrame.from_dict({k:v.get_stats() for k,v in dfuncs_d.items()}).T
+        
+        #write
+        ofp = ses.output({**{'_smry':smry_df}, **clib_d},
+                          ofn='%s_%s.xls'%(os.path.basename(fp).replace('.xls', ''), datetime.datetime.now().strftime('%m%d')),
+                          )
+        
+ 
+        
+    return ofp
+        
+        
+        
+        
+    
+    
+
+
+if __name__ == "__main__": 
+    
+    #output = run_use1()
+    output = add_smry()
+    # reader()
+    
+    tdelta = datetime.datetime.now() - start
+    print('finished in %s \n    %s' % (tdelta, output))
