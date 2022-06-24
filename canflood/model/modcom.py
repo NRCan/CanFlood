4a0eec47856884750f8e5af443902ce00937692a
Seth Bryant <bryant.seth@gmail.com>
2022-06-24 11:52:01 -0600

    Update modcom.py
    cleanup some whitespace and comments

--- canflood/model/modcom.py
+++ canflood/model/modcom.py
@@ -377,10 +377,8 @@ class Model(ComWrkr,
         #=======================================================================
         # attach control file parameter values
         #=======================================================================
-
         self.cfPars_d = self.cf_attach_pars(self.pars)
-        
-        
+
         #=======================================================================
         # #check our validity tag
         #=======================================================================
@@ -402,9 +400,7 @@ class Model(ComWrkr,
         #=======================================================================
         # #wrap
         #=======================================================================
-        self.logger.debug('finished init_modelon Model')
-        
-        
+        self.logger.debug('finished init_model')
 
         
     def cf_attach_pars(self, #load parametersrom file
@@ -710,7 +706,7 @@ class Model(ComWrkr,
     # LOADERS------
     #===========================================================================
     def load_df_ctrl(self,#load raw data from control file
-                     dtag_d=None,
+                     dtag_d=None, #data file loading parameters {key:kwargs for pd.read
                       logger=None,
                       ): 
         #=======================================================================
@@ -789,6 +785,7 @@ class Model(ComWrkr,
         cdf, prefix_l = self._get_finv_cnest(df)
         
         log.info('got %i nests: %s'%(len(prefix_l), prefix_l))
+        
         #=======================================================================
         # mitigation----
         #=======================================================================
