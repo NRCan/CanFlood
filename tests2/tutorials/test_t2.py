d0511a643b1870255376d0a2fb6d3522c59c55f7
cefect <cefect@gmail.com>
2022-06-26 16:42:34 +0200

    Update test_t2.py
    test_04_build_hsamp

--- tests2/tutorials/test_t2.py
+++ tests2/tutorials/test_t2.py
@@ -8,6 +8,8 @@ tutorial 2 integration tests
 
 import pytest, os, shutil
 
+import pandas as pd
+
 from qgis.core import QgsCoordinateReferenceSystem, QgsVectorLayer, QgsProject
 from PyQt5.QtTest import QTest
 from PyQt5.Qt import Qt
@@ -65,15 +67,12 @@ def test_01_build(session, data_dir):
     fp = dial.get_cf_par(dial.get_cf_fp(), sectName='dmg_fps', varName='finv')
     assert os.path.exists(fp)
     
-    """TODO: incorporate additional tests on finv conversion
-        should be able to use the same tests from some future unit test"""
-    
-    
-    
+    df = pd.read_csv(fp)
+    assert len(df)==finv_vlay.dataProvider().featureCount()
+ 
     #===========================================================================
     # Select Vulnerability Function Set
     #===========================================================================
-    
     
     """too tricky to use this UI. should add at somepoint though
     QTest.mouseClick(dial.pushButton_inv_vfunc, Qt.LeftButton)"""
@@ -96,6 +95,38 @@ def test_01_build(session, data_dir):
     
     #update control file
     QTest.mouseClick(dial.pushButton_Inv_curves, Qt.LeftButton)
+    
+    #===========================================================================
+    # hazard sampler
+    #===========================================================================
+    dial._change_tab('tab_HazardSampler')
+    
+    #load rasters
+    lay_d = session.load_layers_dirs([os.path.join(data_dir, 'haz_rast')])
+    
+    QTest.mouseClick(dial.pushButton_expo_refr, Qt.LeftButton) #refresh
+    QTest.mouseClick(dial.pushButton_expo_sAll, Qt.LeftButton) #select All
+    QTest.mouseClick(dial.pushButton_HSgenerate, Qt.LeftButton) #sample
+    
+    #check it
+    fp = dial.get_cf_par(dial.get_cf_fp(), sectName='dmg_fps', varName='expos')
+    assert os.path.exists(fp)
+    
+    df = pd.read_csv(fp, index_col=0)
+    assert len(df)==finv_vlay.dataProvider().featureCount()
+    assert len(df.columns)==len(lay_d)
+    
+    #===========================================================================
+    # event variables
+    #===========================================================================
+    dial._change_tab('tab_eventVars')
+    
+    
+        
+    
+    
+    
+    
     
     
 
