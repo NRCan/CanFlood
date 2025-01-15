9b4dcc639e3813ab4b17b982f03fc7840cf933e8
Seth Bryant <bryant.seth@gmail.com>
2025-01-15 12:32:41 -0700

    Revert "Added logic to handle relative path"
    This reverts commit 313b7a96e274cf017f931d469b54426adabba5d8.

--- canflood/model/modcom.py
+++ canflood/model/modcom.py
@@ -775,7 +775,9 @@ class Model(ComWrkr,
                 log.debug('no \'%s\'... skipping'%dtag)
                 continue
             
-            fp = os.path.join(self.cf_dir, fp)
+            base_dir = os.path.dirname(os.path.dirname(__file__))
+            parent_dir = os.path.dirname(base_dir)
+            fp = os.path.join(parent_dir, fp)
             #check it
             assert os.path.exists(fp), '\'%s\' got bad filepath: \n    %s'%(dtag, fp)
             
@@ -2104,8 +2106,6 @@ class Model(ComWrkr,
                 assert isinstance(pval, str), '%s.%s expected a filepath '%(sect, varnm)
                 if pval == '':
                     raise Error('must provided a valid \'%s.%s\' filepath'%(sect, varnm))
-                if not os.path.exists(pval):
-                    pval = os.path.join(self.cf_dir,pval)
                 assert os.path.exists(pval), '%s.%s passed invalid filepath: \'%s\''%(sect, varnm, pval)
                 
                 ext = os.path.splitext(os.path.split(pval)[1])[1]
